ac4656411b70d5d362c189ff68ab4719fa2e3957
jojotjo <jo.jot.prabh@gmail.com>
2025-08-29 18:05:50 +0530

    Adding Header and Footer Section

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "../snippets/header";
+import Footer from "../snippets/footer";
 const LandingPage = () => {
   return (
     <div>
       <Header/>  
+      <Footer/>
     </div>
   )
 }
