e5d4d22e927e3f79e89dbcec5ed558737070e7a1
jojotjo <jo.jot.prabh@gmail.com>
2026-01-11 16:32:38 +0700

    solve the error in client api

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = process.env.VITE_API_URL;
+const API = import.meta.env.VITE_API_URL;
 
 export const loginUser = (data) => {
   return axios.post(`${API}/auth/login`, data);
