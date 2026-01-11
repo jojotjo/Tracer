588de0dc9267d2e6b10a9b773e026483e889bd05
jojotjo <jo.jot.prabh@gmail.com>
2026-01-11 17:56:26 +0700

    fix: login and sigup routes production

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const API = import.meta.env.VITE_API_URL;
 
 export const loginUser = (data) => {
-  return axios.post(`${API}auth/login`, data);
+  return axios.post(`${API}/auth/login`, data, { withCredentials: true });
 };
 
 export const signupUser = (data) => {
-  return axios.post(`${API}auth/signup`, data);
+  return axios.post(`${API}/auth/signup`, data, { withCredentials: true });
 };
